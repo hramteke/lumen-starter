71ef9bd4b4dd75715cdc8485c33ac0edc57ecbb7
Ben Kuhl <benkuhl@gmail.com>
2016-05-19 12:41:00 -0400

    Postgres default (#47)
    * Added healthcheck endpoint
    
    * Database config defaults to local postgres settings
    
    * Token is now correctly passed to the route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,18 @@ $app->group(['prefix' => 'api/v1'], function () use ($app) {
     $app->post('quotes', ['middleware' => 'jsonApi.enforceMediaType', 'uses' => QuoteController::class . '@post']);
     $app->get('quotes/{id}', ['middleware' => 'jsonApi.enforceMediaType', 'uses' => QuoteController::class . '@show']);
 });
+
+/**
+ * Healthcheck to ensure the application is healthy.  When deployed this endpoint will determine healthy nodes
+ * where the application can live.  In the event of network connectivity failure with any external dependencies,
+ * this healthcheck should fail.
+ */
+$app->get('/healthcheck/{token}', function ($token) {
+    if ($token == env('HEALTHCHECK_TOKEN')) {
+        $connection = DB::connection();
+        $connection->disconnect();
+        return response();
+    } else {
+        throw new \Exception('Invalid healthcheck token');
+    }
+});
